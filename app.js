b8d004ba88b3cdb560f817d25dd19433aee560b4
obi3965 <5571142@RTS-365.dk>
2020-01-07 08:50:33 +0100

    commit

--- app.js
+++ app.js
@@ -38,8 +38,24 @@ const tours = JSON.parse(
 
  app.post('/api/v1/tours', (req,res)=>{
 
-  console.log(req.body);
-  res.send('done')
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  //it will let us to create a new object
+  const newTour = Object.assign({id: newId},req.body);
+
+  //now push it into tours array
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, 
+  JSON.stringify(tours), 
+  err =>{
+    res.status(201).json({
+      status:'success',
+      data:{
+        tour:newTour
+      }
+    })
+  })
+  
  })
 const port = 4000;
 app.listen(port, function(){
